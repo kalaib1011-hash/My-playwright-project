a01bab54ce7587f19230a32061966a5df14b1d2d
kalaib1011-hash <kalaib1022@gmail.com>
2026-07-03 20:34:36 +0530

    remove delete spec

---
+++ tests/delete.spec.js
@@ -0,0 +1,18 @@
+//file created for testing
+import {test} from '@playwright/test'
+//simple dropdown
+test('dropdown', async({page})=>{
+
+await page.goto('https://www.testautomationcentral.com/demo/dropdown.html');
+await page.locator('#simple-dropdown select').selectOption({value:'option3'}); //by value
+await page.locator('#simple-dropdown select').selectOption({label:'Option 1'}); //by text
+await page.locator('#simple-dropdown select').selectOption({index:1}) //by index
+})
+
+//Multiselect dropdown
+test('multiselect',async({page})=>{
+    await page.goto('https://www.testautomationcentral.com/demo/dropdown.html')
+
+    await page.locator('[data-target="multi-select-dropdown"]').click();
+    await page.locator('.form-multiselect').selectOption([{value:'option1'},{index:2},{label:'Option 4'}]);
+})
